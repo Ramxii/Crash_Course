54ff9a8653fc4bdeb7c3ed03a0d91b67d08cb805
Ramxii <ramziemontoute@gmail.com>
2023-05-16 06:19:01 -0400

    ex 2-9 to 2-11

---
+++ chapter_2/adding_comments.py
@@ -0,0 +1,10 @@
+# name_cases.py - prints "Elvis" in different formats
+name = "Elvis"
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+# favorite_number.py - prints the variable number in "print()"
+number = 45
+
+print(f"My favorite number is {number}")
