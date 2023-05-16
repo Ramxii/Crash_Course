8817cd68ca3c85ea2b320ce99d1afafee7cd9347
Ramxii <ramziemontoute@gmail.com>
2023-05-16 06:52:45 -0400

    ex 3-1 to 3-3

---
+++ chapter_3/greetings.py
@@ -0,0 +1,6 @@
+names = ["Melvin", "Stacy", "Peter", "Malcom"]
+
+print(f"Hey {names[0]}, how are you doing today?")
+print(f"Hey {names[1]}, how are you doing today?")
+print(f"Hey {names[2]}, how are you doing today?")
+print(f"Hey {names[3]}, how are you doing today?")
---
+++ chapter_3/your_own_list.py
@@ -0,0 +1,6 @@
+transport = ["Honda", "Susuki", "Toyota", "Mazda"]
+
+print(f"I would like to drive a {transport[0]} truck.")
+print(f"I would like to drive a {transport[1]} truck.")
+print(f"I would like to drive a {transport[2]} truck.")
+print(f"I would like to drive a {transport[3]} truck.")
