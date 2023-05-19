55644a91a9317c96d3e4dc8008e69d20eb86eb05
Ramxii <ramziemontoute@gmail.com>
2023-05-19 06:12:47 -0400

    ex. 5-1

---
+++ chapter_5/conditional_tests.py
@@ -0,0 +1,35 @@
+pizza = 'pepparoni'
+print('pepparoni pizza\n')
+
+print("Is pizza == 'pepparoni'?")
+print(pizza == 'pepparoni')
+
+print("Is pizza != 'pepparoni'?")
+print(pizza != 'pepparoni')
+
+print("Is pizza == 'pineapple'?")
+print(pizza == 'pineapple')
+
+print("Is pizza != 'pineapple'?")
+print(pizza != 'pineapple')
+
+print("Is pizza == 'sausage'?")
+print(pizza == 'sausage')
+
+print("Is pizza != 'sausage'?")
+print(pizza != 'sausage')
+
+print("Is pizza == 'sausage'?")
+print(pizza == 'sausage')
+
+print("Is pizza != 'sausage'?")
+print(pizza != 'sausage')
+
+print("Is pizza != 'bbq chicken'?")
+print(pizza != 'bbq chicken')
+
+print("Is pizza == 'bbq chicken'?")
+print(pizza == 'bbq chicken')
+
+print("Is pizza != 'bbq chicken'?")
+print(pizza != 'bbq chicken')
