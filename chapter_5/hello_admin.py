aaaa4cbab1df1061479e4500e82c58a9e2443eec
Ramxii <ramziemontoute@gmail.com>
2023-05-22 06:46:52 -0400

    ex. 5-8

---
+++ chapter_5/hello_admin.py
@@ -0,0 +1,7 @@
+user_names = ['daniel', 'kayla', 'marvin', 'felise', 'joanna', 'admin']
+
+for user in user_names:
+    if user == 'admin':
+        print("Welcome System Administrator")
+    else:
+        print(f"Hello {user}, thank you for logging in")
