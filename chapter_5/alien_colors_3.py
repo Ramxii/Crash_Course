728870965b2de5fc556f1065affb7892656a46f8
Ramxii <ramziemontoute@gmail.com>
2023-05-22 06:12:54 -0400

    ex 5-5

---
+++ chapter_5/alien_colors_3.py
@@ -0,0 +1,24 @@
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You have earned 5 points")
+elif alien_color == 'yellow':
+    print("You have earned 10 points")
+elif alien_color == 'red':
+    print("You have earned 15 points")
+# version 2
+alien_color = 'yellow'
+if alien_color == 'green':
+    print("You have earned 5 points")
+elif alien_color == 'yellow':
+    print("You have earned 10 points")
+elif alien_color == 'red':
+    print("You have earned 15 points")
+# version 3
+alien_color = 'red'
+if alien_color == 'green':
+    print("You have earned 5 points")
+elif alien_color == 'yellow':
+    print("You have earned 10 points")
+elif alien_color == 'red':
+    print("You have earned 15 points")
