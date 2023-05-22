42708e32671ca0b418c90d02ce149c36df529f65
Ramxii <ramziemontoute@gmail.com>
2023-05-22 06:32:07 -0400

    ex. 5-7

---
+++ chapter_5/favorite_fruit.py
@@ -0,0 +1,12 @@
+favorite_fruits = ['apples', 'bananas', 'oranges']
+
+if 'apples' in favorite_fruits:
+    print("Apples is in the list")
+if 'mangos' in favorite_fruits:
+    print("Mangos is in the list")
+if 'bananas' in favorite_fruits:
+    print("Bananas is in the list")
+if 'grapes' in favorite_fruits:
+    print("Grapes is in the list")
+if 'oranges' in favorite_fruits:
+    print("Oranges is in the list")
