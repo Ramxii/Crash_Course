a1b2dc158b28db73555290f893073627438aa0f3
Ramxii <ramziemontoute@gmail.com>
2023-05-22 06:27:51 -0400

    ex. 5-2

---
+++ chapter_5/more_conditional_tests.py
@@ -0,0 +1,17 @@
+word = 'bird'
+
+# test for equolity with strings
+print(word == 'bird')
+print(word == 'bee')
+# test using the lower() method
+print("\n")
+print(word.lower() == 'Bird')
+print(word.lower() == 'bird')
+# numerical test 
+print("\n")
+print(len(word) == 4)
+print(len(word) <= 3)
+print(len(word) < 4)
+print(len(word) >= 2)
+print(len(word) > 4)
+print(len(word) != 4)
