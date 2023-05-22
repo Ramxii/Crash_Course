86a19f7edfde2c6ee6a7229cbe79d9cf071d7e78
Ramxii <ramziemontoute@gmail.com>
2023-05-22 05:59:29 -0400

    ex. 5-3

---
+++ chapter_5/alien_colors_1.py
@@ -0,0 +1,7 @@
+alien_color = ['green', 'yellow', 'red']
+
+if 'green' in alien_color:
+    print("You have earned 5 points")
+# second version
+if 'white' in alien_color:
+    print("You have earned 5 points")
