c4953377e8ed876a2a0ebbc465611ed1e8fc5a44
Ramxii <ramziemontoute@gmail.com>
2023-05-23 06:05:35 -0400

    ex. 6-1

---
+++ chapter_6/person.py
@@ -0,0 +1,10 @@
+person = {
+    'firstname' : 'Mical',
+    'last_name' : 'Jona',
+    'age' : 43,
+    'city' : 'Los Angeles',
+}
+print(person['firstname'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
