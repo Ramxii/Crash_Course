889a12300c95fff9c52831545ff456e3f1e598d3
Ramxii <ramziemontoute@gmail.com>
2023-05-23 06:39:33 -0400

    ex.6-3

---
+++ chapter_6/glossary.py
@@ -0,0 +1,19 @@
+glossary = {
+    'Conditional statements' : 'evaluate to true or false.',
+    'If statements' : 'runs a block of code based on whether or not a condition is true.',
+    'Loops' : 'check a condition and then run a code block. The loop will continue to check and run until a specified condition is reached.',
+    'Strings' : 'is a sequence of characters and can contain letters, numbers, symbols and even spaces. It must be enclosed in quotation marks for it to be recognized as a string.',
+    'Variable' : 'storing values so that we can reuse them throughout our code or change them, if necessary.',
+}
+
+print(f"Conditional Statemends:\n\t{glossary['Conditional statements']}")
+print("\n")
+print(f"If Statements:\n\t{glossary['If statements']}")
+print("\n")
+print(f"Loops:\n\t{glossary['Loops']}")
+print("\n")
+print(f"Strings:\n\t{glossary['Strings']}")
+print("\n")
+print(f"Variable:\n\t{glossary['Variable']}")
+
+
