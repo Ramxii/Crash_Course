18280995db497627d21def12f9396c8edcb30f41
Ramxii <ramziemontoute@gmail.com>
2023-05-23 06:12:52 -0400

    ex. 6-3

---
+++ chapter_6/favorite_number.py
@@ -0,0 +1,13 @@
+favorite_numbers = {
+    'joana' : 23,
+    'mark' : 55,
+    'lisa' : 12,
+    'melani' : 34,
+    'tom' : 1,
+}
+
+print(f"Joana's favorite number is {favorite_numbers['joana']}")
+print(f"Tom's favorite number is {favorite_numbers['tom']}")
+print(f"Mark's favorite number is {favorite_numbers['mark']}")
+print(f"Lisa's favorite number is {favorite_numbers['lisa']}")
+print(f"Melani's favorite number is {favorite_numbers['melani']}")
