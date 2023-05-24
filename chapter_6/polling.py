953df6bd5c80597ee3747d6e3766197d9b3dbc2c
Ramxii <ramziemontoute@gmail.com>
2023-05-24 06:09:45 -0400

    ex. 6-6

--- chapter_6/polling.py
+++ chapter_6/polling.py
@@ -1,3 +1,4 @@
+poll = ['annie', 'lisa', 'robert', 'kindred', 'darius']
 languages = {
     'annie' : 'python',
     'mark' : 'c',
@@ -6,4 +7,8 @@ languages = {
     'stephanie' : 'java',
 }
 
-for name in 
+for name in poll:
+    if name in languages.keys():
+        print(f"{name} has already taken the poll")
+    else:
+        print(f"{name} you need to take the poll")
