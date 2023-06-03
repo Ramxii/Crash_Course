059fc25a1e9e5e8e778e1923a48da97b699a2136
Ramxii <ramziemontoute@gmail.com>
2023-06-03 05:41:42 -0400

    ex. 8-4

---
+++ chapter_8/large_shirts.py
@@ -0,0 +1,6 @@
+def make_tshirt(size='large', text='I love Python'):
+    """creates shirt with size and text"""
+    print(f"The shirt size is {size.title()} and the text reads '{text.title()}'.")
+
+make_tshirt()
+make_tshirt(size='medium')
