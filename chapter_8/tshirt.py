2af660be785d24fb42f593d3ab36f6e8d05e63c2
Ramxii <ramziemontoute@gmail.com>
2023-06-03 05:37:13 -0400

    ex. 8-3

---
+++ chapter_8/tshirt.py
@@ -0,0 +1,6 @@
+def make_tshirt(size, text):
+    """creates shirt with size and text"""
+    print(f"The shirt size is {size} and the text reads '{text}'.")
+
+make_tshirt(34, 'Bob Marley')
+make_tshirt(size=42, text='Jackson 5')
