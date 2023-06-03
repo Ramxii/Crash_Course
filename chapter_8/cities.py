47c36e67bab16983a9e24ed75caa0d2050a612d0
Ramxii <ramziemontoute@gmail.com>
2023-06-03 05:48:57 -0400

    ex. 8-5

---
+++ chapter_8/cities.py
@@ -0,0 +1,7 @@
+def describe_city(city='castries', country='st. lucia'):
+    """Prints name of city and country"""
+    print(f"{city.title()} is in {country.title()}")
+
+describe_city()
+describe_city(city='kingstown', country='jamaica')
+describe_city(city='london', country='england')
