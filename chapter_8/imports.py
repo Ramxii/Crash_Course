7767fda907fcd3d7675a33db7b3bba7fbba902a1
Ramxii <ramziemontoute@gmail.com>
2023-06-11 08:29:11 -0400

    ex. 8-16

---
+++ chapter_8/imports.py
@@ -0,0 +1,10 @@
+import city_module
+import city_module as cm
+from city_module import describe_city
+from city_module import describe_city as dc
+from city_module import *
+
+describe_city()
+cm.describe_city(city='kingstown', country='jamaica')
+dc(city='london', country='england')
+city_module.describe_city(city='new york', country='america')
