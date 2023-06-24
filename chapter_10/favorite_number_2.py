7b7d2336e3bcb15b13e3203b300ea5aec69c7e83
Ramxii <ramziemontoute@gmail.com>
2023-06-24 06:23:11 -0400

    ex. 10-11

---
+++ chapter_10/favorite_number_2.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+import json
+
+path = Path("favorite_number.json")
+contents = path.read_text()
+favorite_number = json.loads(contents)
+
+print(f"I know your favorite number is {favorite_number}")
